249c2d5c50a882ccf38d682da9f44214e7ad61dd
Andrew Ligon <andrew.ligon@nextthought.com>
2018-08-17 15:10:38 -0500

    leave the existing api

--- src/decorators/Searchable.jsx
+++ src/decorators/Searchable.jsx
@@ -84,8 +84,108 @@ export class Searchable extends React.Component {
 	}
 }
 
-export function searchable (scope) {
-	if (typeof scoped !== 'string') { throw new Error('The only argument to the searchable decorator is the scope to search in'); }
+
+class SearchableStore extends React.Component {
+
+	/**
+	 * Used to compose a Component Class. This returns a new Component Type.
+	 *
+	 * This is intended to be incorporated into a store's connect() decorator,
+	 * and not meant to be used directly as a decorator.
+	 *
+	 * @param  {Object} store The store to connect to.
+	 * @param  {Class} component The component to compose & wire to store updates.
+	 * @param  {Object} propMap mapping of key from store to a a prop name.
+	 *                          Ex:
+	 *                          {
+	 *                              'AppUser': 'user',
+	 *                              'AppName': 'title',
+	 *                          }
+	 * @param  {Function} onMount A callback after the component mounts. Handy to dynamically build stores or load data.
+	 * @param  {Function} onUnmount A callback before the component unmounts.
+	 * @return {Function} A Composed Component
+	 */
+	static connect (store, component, propMap, onMount, onUnmount) {
+		const cmp = React.forwardRef((props, ref) => (
+			<Searchable
+				{...props}
+				_forwardedRef={ref}
+				_store={store}
+				_propMap={propMap}
+				_component={component}
+				_onMount={onMount}
+				_onUnmount={onUnmount}
+			/>
+		));
+
+		return HOC.hoistStatics(cmp, component, 'SearchableStoreConnector');
+	}
+
+	static propTypes = {
+		_forwardedRef: PropTypes.any,
+		_store: PropTypes.object
+	}
+
+
+	constructor (props) {
+		super(props);
+
+		this.searchStore = SearchStore.getGlobal();
+	}
+
+
+	componentDidMount () {
+		this.searchStore.addChangeListener(this.onStoreChange);
+	}
+
+
+	componentWillUnmount () {
+		this.searchStore.removeChangeListener(this.onStoreChange);
+	}
+
+
+	updateSearchTerm () {
+		const {_store} = this.props;
+
+		if (_store && _store.updateSearchTerm) {
+			_store.updateSearchTerm(this.searchStore.searchTerm);
+		}
+	}
+
+
+	onStoreChange = ({type}) => {
+		if (type === 'searchTerm') {
+			this.updateSearchTerm();
+		}
+	}
+
+
+	render () {
+		const {
+			_forwardedRef,
+			...otherProps
+		} = this.props;
+
+		return (
+			<StoreConnector {...otherProps} ref={_forwardedRef}/>
+		);
+	}
+}
+
+
+export function searchable (scope, propMap) {
+	if (typeof scoped !== 'string') {
+		return (component) => {
+			 // I want this warning to be visible no matter what...hence, the disabled lint line.
+		    // DO NOT do this for your debug console logging.
+			console.warn( //eslint-disable-line no-console
+				'Do not use @searchable() decorator, use a store’s connect decorator instead.\nCaused by: %o',
+				component.name || component.displayName || component
+			);
+
+			return SearchableStore.connect(scope, component, propMap);
+		}
+	}
 
 	return (Component) => {
 		const cmp = React.forwardRef((props, ref) => {
