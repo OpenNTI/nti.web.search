9b66e09a1ad0f06a828b338c1c45e441603e1fab
Andrew Ligon <andrew.ligon@nextthought.com>
2018-08-17 14:07:22 -0500

    Update the searchable mixin

--- src/decorators/Searchable.jsx
+++ src/decorators/Searchable.jsx
@@ -1,112 +1,104 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {HOC} from '@nti/lib-commons';
-import StoreConnector from '@nti/lib-store-connector';
 
 import SearchStore from '../Store';
 
+function getSearchStore (scope) {
+	if (scope) {
+		return SearchStore.getForScope(scope);
+	}
+
+	return SearchStore.getGlobal();
+}
+
 export class Searchable extends React.Component {
-
-	/**
-	 * Used to compose a Component Class. This returns a new Component Type.
-	 *
-	 * This is intended to be incorporated into a store's connect() decorator,
-	 * and not meant to be used directly as a decorator.
-	 *
-	 * @param  {Object} store The store to connect to.
-	 * @param  {Class} component The component to compose & wire to store updates.
-	 * @param  {Object} propMap mapping of key from store to a a prop name.
-	 *                          Ex:
-	 *                          {
-	 *                              'AppUser': 'user',
-	 *                              'AppName': 'title',
-	 *                          }
-	 * @param  {Function} onMount A callback after the component mounts. Handy to dynamically build stores or load data.
-	 * @param  {Function} onUnmount A callback before the component unmounts.
-	 * @return {Function} A Composed Component
-	 */
-	static connect (store, component, propMap, onMount, onUnmount) {
-		const cmp = React.forwardRef((props, ref) => (
-			<Searchable
-				{...props}
-				_forwardedRef={ref}
-				_store={store}
-				_propMap={propMap}
-				_component={component}
-				_onMount={onMount}
-				_onUnmount={onUnmount}
-			/>
-		));
-
-		return HOC.hoistStatics(cmp, component, 'SearchableStoreConnector');
-	}
-
 	static propTypes = {
-		_forwardedRef: PropTypes.any,
-		_store: PropTypes.object
+		scope: PropTypes.string,
+		children: PropTypes.element
 	}
 
-
-	constructor (props) {
-		super(props);
-
-		this.searchStore = SearchStore.getGlobal();
-	}
+	state = {}
 
 
 	componentDidMount () {
-		this.searchStore.addChangeListener(this.onStoreChange);
+		this.setupFor(this.props);
 	}
 
 
-	componentWillUnmount () {
-		this.searchStore.removeChangeListener(this.onStoreChange);
-	}
+	componentDidUpdate (prevProps) {
+		const {scope} = this.props;
+		const {scope:prevScope} = prevProps;
 
-
-	updateSearchTerm () {
-		const {_store} = this.props;
-
-		if (_store && _store.updateSearchTerm) {
-			_store.updateSearchTerm(this.searchStore.searchTerm);
+		if (prevScope !== scope) {
+			this.setupFor(this.props);
 		}
 	}
 
 
-	onStoreChange = ({type}) => {
-		if (type === 'searchTerm') {
-			this.updateSearchTerm();
+	componentWillUnmount () {
+		this.unmounted = true;
+
+		if (this.unsubscribe) {
+
+		}
+	}
+
+
+	setupFor (props) {
+		const {scope} = this.props;
+		const store = getSearchStore(scope);
+
+		this.setState({
+			store
+		});
+
+		if (this.unsubscribe) {
+			this.unsubscribe();
+		}
+
+		store.addChangeListener(this.onStoreChange);
+
+		this.unsubscribe = () => {
+			store.removeChangeListener(this.onStoreChange);
+			delete this.unsubscribe;
+		};
+	}
+
+
+	onStoreChange = () => {
+		if (!this.unmounted) {
+			this.forceUpdate();
 		}
 	}
 
 
 	render () {
-		const {
-			_forwardedRef,
-			...otherProps
-		} = this.props;
+		const {children} = this.props;
+		const {store} = this.state;
+		const searchTerm = store ? store.searchTerm : null;
 
 		return (
-			<StoreConnector {...otherProps} ref={_forwardedRef}/>
+			React.cloneElement(React.Children.only(children), {searchTerm});
 		);
 	}
 }
 
+export function searchable (scope) {
+	if (typeof scoped !== 'string') { throw new Error('The only argument to the searchable decorator is the scope to search in'); }
 
-/**
- * @deprecated Use Searchable.connect() in a store's connect decorator instead.
- * @method searchable
- * @param  {Object}   store   The store that implements the searchable interfaces
- * @param  {Object}   propMap A mapping of store values to propNames to apply to the component.
- * @return {Function} A decorator that returns a Composed Component
- */
-export function searchable (store, propMap) {
-	return (component) => (
-		// I want this warning to be visible no matter what...hence, the disabled lint line.
-		// DO NOT do this for your debug console logging.
-		console.warn( //eslint-disable-line no-console
-			'Do not use @searchable() decorator, use a store’s connect decorator instead.\nCaused by: %o',
-			component.name || component.displayName || component
-		),
-		Searchable.connect(store, component, propMap));
+	return (Component) => {
+		const cmp = React.forwardRef((props, ref) => {
+			return (
+				<Searchable scope={scope}>
+					<Component ref={ref} />
+				</Searchable>
+			);
+		});
+
+
+		HOC.hoistStatics(cmp, Component, 'Searchable');
+
+		return cmp;
+	}
 }
